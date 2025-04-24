1c302cf769de70a68d5c9efd728d35ba2ccf5504
Navya Gupta <navyagupta171@gmail.com>
2025-04-25 00:04:27 +0530

    image path modified

--- src/data/cryptodata.tsx
+++ src/data/cryptodata.tsx
@@ -1,51 +1,51 @@
 const cryptoMeta = {
     BTCUSDT: {
       name: 'Bitcoin',
-      logo: '/src/assets/bitcoin.png',
+      logo: '/assets/bitcoin.png',
         Oh:'0.43',
         sevenD:'23.44',
       circulatingSupply: '19,600,000',
       marketCap: '21,000,000,344',
-      graph:'/src/assets/g1.png'
+      graph:'/assets/g1.png'
     },
     ETHUSDT: {
       name: '.Ethereum',
-      logo: '/src/assets/ere.jpg',
+      logo: '/assets/ere.jpg',
       Oh:'0.53',
       sevenD:'20.44',
       circulatingSupply: '120,200,000',
       marketCap: '121,567,890,332',
-      graph:'/src/assets/g1.png'
+      graph:'/assets/g1.png'
     },
     USDTUSDT: {
       name: 'Tether',
-      logo: '/src/assets/te.jpg',
+      logo: '/assets/te.jpg',
       Oh:'0.0',
       sevenD:'0.0',
     
       circulatingSupply: '83,000,000,000',
       marketCap: '124,567,765,444',
-      graph:'/src/assets/g1.png'
+      graph:'/assets/g1.png'
     },
     XRPUSDT: {
       name: 'XRP',
-      logo: '/src/assets/xrp.jpg',
+      logo: '/assets/xrp.jpg',
       Oh:'0.23',
       sevenD:'28.29',
     
       circulatingSupply: '53,000,000,000',
       marketCap: '100,000,000,000',
-      graph:'/src/assets/g1.png'
+      graph:'/assets/g1.png'
     },
     SOLUSDT: {
       name: 'Solana',
-      logo: '/src/assets/sol.jpg',
+      logo: '/assets/sol.jpg',
       Oh:'0.48',
       sevenD:'53.64',
     
       circulatingSupply: '430,000,000',
       marketCap: '123,232,323,333',
-      graph:'/src/assets/g1.png'
+      graph:'/assets/g1.png'
     },
   };
   
