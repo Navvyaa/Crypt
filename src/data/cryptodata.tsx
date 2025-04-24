87134282cea8c11b9b0e262501a7f8bc35ee6583
Navya Gupta <navyagupta171@gmail.com>
2025-04-24 20:12:22 +0530

    initialized git repo

---
+++ src/data/cryptodata.tsx
@@ -0,0 +1,35 @@
+const cryptoMeta = {
+    BTCUSDT: {
+      name: 'Bitcoin',
+      logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      circulatingSupply: '19,600,000',
+      maxSupply: '21,000,000',
+    },
+    ETHUSDT: {
+      name: 'Ethereum',
+      logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png',
+      circulatingSupply: '120,200,000',
+      maxSupply: 'No Limit',
+    },
+    USDTUSDT: {
+      name: 'Tether',
+      logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png',
+      circulatingSupply: '83,000,000,000',
+      maxSupply: 'No Limit',
+    },
+    XRPUSDT: {
+      name: 'XRP',
+      logo: 'https://cryptologos.cc/logos/xrp-xrp-logo.png',
+      circulatingSupply: '53,000,000,000',
+      maxSupply: '100,000,000,000',
+    },
+    SOLUSDT: {
+      name: 'Solana',
+      logo: 'https://cryptologos.cc/logos/solana-sol-logo.png',
+      circulatingSupply: '430,000,000',
+      maxSupply: 'No Limit',
+    },
+  };
+  
+  export default cryptoMeta;
+  
