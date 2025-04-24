9a6889b37406ee119691407a1115f67c6a9b6900
Navya Gupta <navyagupta171@gmail.com>
2025-04-24 23:59:51 +0530

    fixed error

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import  { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateCrypto } from '../redux/cryptoSlice';
 import { throttle } from 'lodash';
